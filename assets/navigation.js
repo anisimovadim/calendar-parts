82ef04bb5ca8d15ea8ed4711994e87b06da2e298
anisimovadim <anisimov.vadim.alexeevich@mail.ru>
2022-12-25 14:55:51 +0300

    first commit

---
+++ assets/navigation.js
@@ -0,0 +1,6 @@
+let navigation = document.getElementById("poloski")
+let navContent = document.getElementById("navContent")
+
+navigation.onclick=function (){
+    navContent.classList.toggle("open_nav")
+}
